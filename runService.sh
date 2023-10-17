c92fa19f248015cf65a79f4bd98462515ba253aa
Fitriani Nasir <fitrianinasir8@gmail.com>
2023-10-17 20:20:24 +0700

    add shell script

--- runService.sh
+++ runService.sh
@@ -1,1 +1,13 @@
-CD /D target
+cd target
+
+# Run the Java application and disown it
+java -jar practice-0.0.1-SNAPSHOT.jar && disown
+
+# Function to handle Ctrl+C
+ctrl_c() {
+    echo "Ctrl+C pressed. Script will now exit."
+    exit 1
+}
+
+# Trap Ctrl+C to call the ctrl_c function
+trap ctrl_c INT
