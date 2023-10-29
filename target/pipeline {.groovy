46bb70800669aa08e0a6a56a574f411ce5ddfa67
Fitriani Nasir <fitrianinasir8@gmail.com>
2023-10-29 19:40:52 +0700

    add pipeline script

---
+++ target/pipeline {.groovy
@@ -0,0 +1,32 @@
+pipeline {
+  agent any
+  environment {
+    DOCKERHUB_CREDS = credentials('dockerhub')
+  }
+  stages {
+    stage('Clone Repo'){
+      steps{
+        echo 'Copy Repository...'
+        checkout scmGit(branches: [[name: '*/dev']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/fitrianinasir/SampleCode-for-Pipeline-testing']])
+        sh 'ls *'
+      }
+    }
+    stage('Build Image'){
+      steps {
+        echo 'Start building the image...'
+        sh 'docker build -t fitrianinasir/first-docker:$BUILD_NUMBER ./target/'
+      }
+    }
+    stage('Docker Login'){
+      steps{
+        echo 'Start login...'
+        sh 'echo $DOCKERHUB_CREDS_PSW | docker login -u $DOCKERHUB_CREDS_USR --password-stdin'
+      }
+    }
+    stage('Docker Push'){
+      steps{
+        sh 'docker push fitrianinasir/first-docker:$BUILD_NUMBER'
+      }
+    }
+  }
+}
